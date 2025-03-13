06524614fed7b4dfba7e144471091829c119c926
barsum bahiraei <barsum.bahiraei@gmail.com>
2025-03-13 19:53:41 +0330

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: '2025-03-13',
     devtools: {enabled: true},
-    css: ['~/assets/scss/main.scss'],
-    vite: {
-        plugins: [tailwindcss()],
-    },
     modules: [
         '@nuxt/eslint',
         '@nuxt/image',
