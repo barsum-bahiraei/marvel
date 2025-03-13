ab2f699d338a2c91ebcdbf69f4ac910867ee9a7c
barsum bahiraei <barsum.bahiraei@gmail.com>
2025-03-13 18:50:54 +0330

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="">
-    <h1>
-      this is Index page
+  <div class="bg-primary-700">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
     </h1>
   </div>
 </template>
