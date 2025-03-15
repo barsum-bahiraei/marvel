c35b518d76e9e752671bfd42873cd0a25b268d80
barsum bahiraei <barsum.bahiraei@gmail.com>
2025-03-15 21:20:29 +0330

    .

--- components/character-film.vue
+++ components/character-film.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink :to="props.id ? `/detail/${props.id}` : ''" class="text-white border border-secondary-2 rounded-xl p-3">
-    <img :src="thumbnailUrl" :alt="props.name" class="w-full rounded-lg">
+    <NuxtPicture :src="thumbnailUrl" :alt="props.name" class="w-full rounded-lg" />
     <span class="mt-3 inline-block">{{ props.name }}</span>
   </NuxtLink>
 </template>
