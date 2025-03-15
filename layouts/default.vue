e50df448b38e0180b4df08b668f61d24febb4b7b
barsum bahiraei <barsum.bahiraei@gmail.com>
2025-03-15 11:07:29 +0330

    first commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-    <AppHeader/>
+  <main>
+    <header class="bg-header-gradient h-[192px]">
+      <div class="container px-3 mx-auto py-4">
+        <div>
+          <a href="" class="bg-danger text-white py-2 px-3 rounded inline-block">LOGO</a>
+        </div>
+        <slot name="header"/>
+      </div>
+    </header>
+
     <slot/>
-  </div>
+  </main>
 </template>
 <script setup lang="ts">
-import AppHeader from "~/layouts/app-header.vue";
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,15 @@
 <template>
-  <div>
-    <div class="bg-header-gradient h-[192px]">
-      <div class="container px-3 mx-auto py-4">
-        <div>
-          <a href="" class="bg-danger text-white py-2 px-3 rounded inline-block">LOGO</a>
-        </div>
-        <div class="bg-secondary p-3 rounded flex mt-8">
-          <input
-              class="bg-secondary-1 placeholder-white w-full rounded px-3 block"
-              type="text"
-              placeholder="Search for characters..."
-          >
-          <button class="bg-danger text-white py-2 px-3 rounded ml-3">Search</button>
-        </div>
+  <NuxtLayout>
+    <template #header>
+      <div class="bg-secondary p-3 rounded flex mt-8">
+        <input
+            class="bg-secondary-1 placeholder-white w-full rounded px-3 block"
+            type="text"
+            placeholder="Search for characters..."
+        >
+        <button class="bg-danger text-white py-2 px-3 rounded ml-3">Search</button>
       </div>
-    </div>
+    </template>
     <div class="bg-secondary py-8">
       <div class="container px-3 mx-auto grid md:grid-cols-4 gap-6">
         <NuxtLink v-for="item in 10" :key="item" to="/detail" class="text-white border border-secondary-2 rounded p-3">
@@ -24,10 +19,14 @@
       </div>
       <PageNumber :total-items="1000" :current-page="count" :items-per-page="20" @page-change="(page)=>count = page"/>
     </div>
-  </div>
+  </NuxtLayout>
 </template>
 <script setup lang="ts">
 import PageNumber from "~/components/page-number.vue";
 
 const count = ref(1)
+
+definePageMeta({
+  layout: 'default', // Or any other layout name
+})
 </script>
