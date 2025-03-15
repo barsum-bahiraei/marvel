51d61cef8f42557c49168892c0112da556eb52fa
barsum bahiraei <barsum.bahiraei@gmail.com>
2025-03-15 17:01:49 +0330

    .

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <header class="bg-header-gradient">
       <div class="container px-3 mx-auto py-4">
         <div class="mb-10">
-          <a href="" class="bg-danger text-white py-2 px-3 rounded inline-block">LOGO</a>
+          <a href="/" class="bg-danger text-white py-2 px-3 rounded inline-block">LOGO</a>
         </div>
         <slot name="header"/>
       </div>
