0a87e42337d03e1c5178ca448ee5926e91342765
barsum bahiraei <barsum.bahiraei@gmail.com>
2025-03-15 21:02:52 +0330

    .

--- components/character-film.vue
+++ components/character-film.vue
@@ -1,13 +1,17 @@
 <template>
   <NuxtLink :to="props.id ? `/detail/${props.id}` : ''" class="text-white border border-secondary-2 rounded-xl p-3">
-    <NuxtImg :src="`${props.thumbnail}/portrait_xlarge.jpg`" :alt="props.name" class="w-full rounded-lg" loading="lazy"/>
+    <NuxtImg :src="thumbnailUrl" :alt="props.name" class="w-full rounded-lg" loading="lazy"/>
     <span class="mt-3 inline-block">{{ props.name }}</span>
   </NuxtLink>
 </template>
 <script lang="ts" setup>
+import {computed} from "vue";
+
 const props = defineProps<{
   name: string;
   thumbnail: string;
   id?: number;
 }>();
+
+const thumbnailUrl = computed<string>(() => `${props.thumbnail}/portrait_xlarge.jpg`)
 </script>
